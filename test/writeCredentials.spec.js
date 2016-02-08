ee62ef44cce31f02f49df4bc79406e2e118eb3e7
Rafael Pedrola <rafinskipg@gmail.com>
2016-02-08 14:30:38 +0100

    started tests

---
+++ test/writeCredentials.spec.js
@@ -0,0 +1,18 @@
+import test from 'tape'
+import writeCredentials from '../src/writeCredentials'
+import fs from 'fs'
+
+test('writeCredentials', (t) => {
+  t.plan(3)
+  var filePath = './.tmp/pepe';
+
+  writeCredentials(filePath, { hola : 'pepe'}, function(err){
+    t.equal(err, null, 'Does not return an error');
+
+    var fileExists = fs.existsSync(filePath);
+    t.equal(fileExists, true, 'The file is written');
+
+    var fileBody = fs.readFileSync(filePath, 'utf-8');
+    t.equal(fileBody, 'hola: pepe\n', 'The file contains yaml');
+  });
+})

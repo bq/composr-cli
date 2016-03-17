76be7b5e64634efc728044dcaa5dac0f9d37d4c4
jscMR <jorge.serrano@bq.com>
2016-03-17 19:28:57 +0100

    publish

--- dist/publish.js
+++ dist/publish.js
@@ -24,6 +24,14 @@ var _jsonSchemaGenerator = require('json-schema-generator');
 
 var _jsonSchemaGenerator2 = _interopRequireDefault(_jsonSchemaGenerator);
 
+var _async = require('async');
+
+var _async2 = _interopRequireDefault(_async);
+
+var _existsFile = require('exists-file');
+
+var _existsFile2 = _interopRequireDefault(_existsFile);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /**
@@ -41,35 +49,86 @@ var buildPhrase = function buildPhrase(modelFilePath, next) {
   var codehash = undefined;
 
   // cli.ok('building phrase:', phraseName)
-  _simpleSpinner2.default.start();
+  // spinner.start()
+  var sourceCodeListExec = [];
+
   (0, _glob2.default)(phraseDir + '/*.code.js', null, function (err, files) {
     if (err) return _cli2.default.error(err);
     files.forEach(function (file) {
-      model = modelGeneration(file, phraseDir, phraseName, model, phraseDirTmp);
+      sourceCodeListExec.push(function (callback) {
+        modelGeneration(file, phraseDir, phraseName, model, phraseDirTmp, function (err, result) {
+          if (err) return callback(err, null);
+
+          var fileNameModel = phraseDirTmp + phraseName + '.model.json';
+
+          // sacar para otra funcion, añadir comprobaciones de respuestas y schema y middlewares
+          (0, _mkdirp2.default)(phraseDirTmp, function (err) {
+            if (err) return _cli2.default.error(err);
+            _fs2.default.writeFileSync(fileNameModel, JSON.stringify(model, null, '\t'));
+            // spinner.stop()
+            return callback(null, result);
+          });
+        });
+      });
     });
-
-    var fileNameModel = phraseDirTmp + phraseName + '.model.json';
-
-    // sacar para otra funcion, añadir comprobaciones de respuestas y schema y middlewares
-    (0, _mkdirp2.default)(phraseDirTmp, function (err) {
-      if (err) return _cli2.default.error(err);
-      _fs2.default.writeFileSync(fileNameModel, JSON.stringify(model, null, '\t'));
-      _simpleSpinner2.default.stop();
-      return next(model);
+    // execution in parallel to build phrase model
+    _async2.default.parallel(sourceCodeListExec, function (err, models) {
+      return next(err, models);
     });
   });
 };
+
 /**
  * Code Generation
  */
-var modelGeneration = function modelGeneration(file, phraseDir, phraseName, model, phraseDirTmp) {
+var modelGeneration = function modelGeneration(file, phraseDir, phraseName, model, phraseDirTmp, next) {
   var fileSplit = file.split('.');
   var verb = fileSplit[fileSplit.length - 3];
 
   var codeFilePath = process.cwd() + '/' + phraseDir + phraseName + '.' + verb + '.code.js';
   // If file exists continue if not return
-  if (!_fs2.default.accessSync(codeFilePath)) return;
-  return model;
+  var exists = (0, _existsFile2.default)(codeFilePath);
+  if (!exists) return next('not exists', null);
+  // continuar la ejecucion
+  _cli2.default.ok('>> Reading file', codeFilePath);
+
+  model[verb] = model[verb] || {};
+  model[verb].doc = model[verb].doc || {};
+  model[verb].doc.responses = model[verb].doc.responses || {};
+
+  var code = _fs2.default.readFileSync(codeFilePath, 'utf8');
+
+  model[verb].codehash = new Buffer(code).toString('base64');
+
+  (0, _glob2.default)(phraseDir + '/*.' + verb + '.response.*[0-9].json', null, function (err, files) {
+    if (err) return _cli2.default.error(err);
+    files.forEach(function (responseFileName) {
+      // responseFileName example:
+      // 'src/phraseName/phraseName.get.response.401.json'
+      var statusCode = responseFileName.split('.');
+      statusCode = statusCode[statusCode.length - 2];
+
+      var responseExample = require('./' + responseFileName);
+      var schema = undefined;
+      try {
+        schema = require('./' + responseFileName.replace('.json', '.schema.json'));
+      } catch (e) {
+        _cli2.default.ok('autogenerate schema for', responseFileName);
+        schema = (0, _jsonSchemaGenerator2.default)(responseExample);
+        schema.$schema = schema.$schema.replace('http://json-schema.org/draft-04/schema#', 'http://json-schema.org/schema');
+        _fs2.default.writeFileSync(phraseDirTmp + phraseName + '.' + verb + '.response.' + statusCode + '.schema.json', JSON.stringify(schema, null, '\t'), 'utf8');
+      }
+
+      model[verb].doc.responses[statusCode] = model[verb].doc.responses[statusCode] || {};
+      model[verb].doc.responses[statusCode].body = model[verb].doc.responses[statusCode].body || {};
+      model[verb].doc.responses[statusCode].body['application/json'] = model[verb].doc.responses[statusCode].body['application/json'] || {};
+      model[verb].doc.responses[statusCode].body['application/json'].schema = JSON.stringify(schema, null, '\t');
+      model[verb].doc.responses[statusCode].body['application/json'].example = JSON.stringify(responseExample, null, '\t');
+    });
+
+    // Final
+    return next(null, model);
+  });
 };
 
 /**
@@ -80,15 +139,27 @@ var locateModels = function locateModels() {
     if (err) return _cli2.default.error(err);
     _cli2.default.ok('MODELS LOCATED');
     var phraseURLs = [];
+    // bulk execution
+    var buildPhrasesExecList = [];
     files.forEach(function (filePath) {
-      buildPhrase(filePath, function (model) {
-        if (phraseURLs.indexOf(model.url) !== -1) {
-          _cli2.default.error('Phrase duplicated [' + model.url + '] ' + filePath);
-        }
-        phraseURLs.push(model.url);
+      buildPhrasesExecList.push(function (callback) {
+        buildPhrase(filePath, function (model) {
+          var response = { model: model, filePath: filePath };
+          callback(null, response);
+        });
       });
     });
-    return console.log(phraseURLs);
+
+    _async2.default.parallel(buildPhrasesExecList, function (err, results) {
+      if (err) return _cli2.default.error(err);
+      results.forEach(function (result) {
+        if (phraseURLs.indexOf(result.model.url) !== -1) {
+          _cli2.default.error('Phrase duplicated [' + result.model.url + '] ' + result.filePath);
+        }
+        phraseURLs.push(result.model.url);
+      });
+      return console.log(phraseURLs);
+    });
   });
 };
 
--- src/publish.js
+++ src/publish.js
@@ -5,6 +5,8 @@ import spinner from 'simple-spinner'
 import fs from 'fs'
 import mkdirp from 'mkdirp'
 import jsonSchemaGenerator from 'json-schema-generator'
+import async from 'async'
+import existsFile from 'exists-file'
 /**
  * ------------------------------
  * Build Phrase
@@ -20,35 +22,93 @@ const buildPhrase = (modelFilePath, next) => {
   let codehash
 
   // cli.ok('building phrase:', phraseName)
-  spinner.start()
+  // spinner.start()
+  let sourceCodeListExec = []
+
   glob(phraseDir + '/*.code.js', null, (err, files) => {
     if (err) return cli.error(err)
     files.forEach((file) => {
-      model = modelGeneration(file, phraseDir, phraseName, model, phraseDirTmp)
+      sourceCodeListExec.push((callback) => {
+        modelGeneration(file, phraseDir, phraseName, model, phraseDirTmp, (err, result) => {
+          if (err) return callback(err, null)
+
+          let fileNameModel = phraseDirTmp + phraseName + '.model.json'
+
+          // sacar para otra funcion, añadir comprobaciones de respuestas y schema y middlewares
+          mkdirp(phraseDirTmp, (err) => {
+            if (err) return cli.error(err)
+            fs.writeFileSync(fileNameModel, JSON.stringify(model, null, '\t'))
+            // spinner.stop()
+            return callback(null, result)
+          })
+        })
+      })
     })
-
-    let fileNameModel = phraseDirTmp + phraseName + '.model.json'
-
-    // sacar para otra funcion, añadir comprobaciones de respuestas y schema y middlewares
-    mkdirp(phraseDirTmp, (err) => {
-      if (err) return cli.error(err)
-      fs.writeFileSync(fileNameModel, JSON.stringify(model, null, '\t'))
-      spinner.stop()
-      return next(model)
+    // execution in parallel to build phrase model
+    async.parallel(sourceCodeListExec, (err, models) => {
+      return next(err, models)
     })
   })
 }
+
 /**
  * Code Generation
  */
-const modelGeneration = (file, phraseDir, phraseName, model, phraseDirTmp) => {
+const modelGeneration = (file, phraseDir, phraseName, model, phraseDirTmp, next) => {
   let fileSplit = file.split('.')
   let verb = fileSplit[fileSplit.length - 3]
 
   let codeFilePath = process.cwd() + '/' + phraseDir + phraseName + '.' + verb + '.code.js'
   // If file exists continue if not return
-  if (!fs.accessSync(codeFilePath)) return
-  return model
+  let exists = existsFile(codeFilePath)
+  if (!exists) return next('not exists', null)
+  // continuar la ejecucion
+  cli.ok('>> Reading file', codeFilePath)
+
+  model[verb] = model[verb] || {}
+  model[verb].doc = model[verb].doc || {}
+  model[verb].doc.responses = model[verb].doc.responses || {}
+
+  let code = fs.readFileSync(codeFilePath, 'utf8')
+
+  model[verb].codehash = new Buffer(code).toString('base64')
+
+  glob(phraseDir + '/*.' + verb + '.response.*[0-9].json', null, (err, files) => {
+    if (err) return cli.error(err)
+    files.forEach((responseFileName) => {
+      // responseFileName example:
+      // 'src/phraseName/phraseName.get.response.401.json'
+      let statusCode = responseFileName.split('.')
+      statusCode = statusCode[statusCode.length - 2]
+
+      let responseExample = require('./' + responseFileName)
+      let schema
+      try {
+        schema = require('./' + responseFileName.replace('.json', '.schema.json'))
+      } catch (e) {
+        cli.ok('autogenerate schema for', responseFileName)
+        schema = jsonSchemaGenerator(responseExample)
+        schema.$schema = schema.$schema.replace(
+          'http://json-schema.org/draft-04/schema#',
+          'http://json-schema.org/schema'
+        )
+        fs.writeFileSync(phraseDirTmp + phraseName + '.' + verb + '.response.' +
+          statusCode + '.schema.json', JSON.stringify(schema, null, '\t'), 'utf8')
+      }
+
+      model[verb].doc.responses[statusCode] = model[verb].doc.responses[statusCode] || {}
+      model[verb].doc.responses[statusCode].body = model[verb].doc.responses[statusCode].body || {}
+      model[verb].doc.responses[statusCode].body['application/json'] =
+        model[verb].doc.responses[statusCode].body['application/json'] || {}
+      model[verb].doc.responses[statusCode].body['application/json'].schema =
+        JSON.stringify(schema, null, '\t')
+      model[verb].doc.responses[statusCode].body['application/json'].example =
+        JSON.stringify(responseExample, null, '\t')
+    })
+
+    // Final
+    return next(null, model)
+  })
 }
 
 /**
@@ -59,15 +119,27 @@ const locateModels = () => {
     if (err) return cli.error(err)
     cli.ok('MODELS LOCATED')
     let phraseURLs = []
+    // bulk execution
+    let buildPhrasesExecList = []
     files.forEach(filePath => {
-      buildPhrase(filePath, (model) => {
-        if (phraseURLs.indexOf(model.url) !== -1) {
-          cli.error('Phrase duplicated [' + model.url + '] ' + filePath)
-        }
-        phraseURLs.push(model.url)
+      buildPhrasesExecList.push((callback) => {
+        buildPhrase(filePath, (model) => {
+          let response = {model: model, filePath: filePath}
+          callback(null, response)
+        })
       })
     })
-    return console.log(phraseURLs)
+
+    async.parallel(buildPhrasesExecList, (err, results) => {
+      if (err) return cli.error(err)
+      results.forEach(result => {
+        if (phraseURLs.indexOf(result.model.url) !== -1) {
+          cli.error('Phrase duplicated [' + result.model.url + '] ' + result.filePath)
+        }
+        phraseURLs.push(result.model.url)
+      })
+      return console.log(phraseURLs)
+    })
   })
 }
 

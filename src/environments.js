842248cda402ff220266a1ffe90e7b6e11b1b464
jscMR <jorge.serrano@bq.com>
2016-04-14 09:56:55 +0200

    added publisher

--- src/environments.js
+++ src/environments.js
@@ -7,7 +7,13 @@ const envManager = (config, next) => {
     config.environments.forEach(env => {
         envs.push({
             name: env.name,
-            composrEndpoint: env.urlBase.replace('{{module}}', 'composr').replace('v1.0/', '')
+            composrEndpoint: env.urlBase.replace('{{module}}', 'composr').replace('v1.0/', ''),
+            credentials : {
+            	clientId : env.clientId,
+            	scopes : env.scopes,
+            	urlBase: env.urlBase,
+            	clientSecret: env.clientSecret
+            }
         })
     })
 
